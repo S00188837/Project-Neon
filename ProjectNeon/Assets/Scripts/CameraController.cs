f19e405e089a1cfe14554b26f75f90557c831d85
DomasRuginis <47103194+S00188837@users.noreply.github.com>
2020-03-02 20:07:11 +0000

    Started PlayerMovevemt

---
+++ ProjectNeon/Assets/Scripts/CameraController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    float minimumX = -60f;
+    float maximumX = 60f;
+    float minimumY = -360f;
+    float maximumY = 360f;
+
+    public float sensitivityX, sensitivityY;
+
+    public Camera cam;
+
+    float rotationY = 0f;
+    float rotationX = 0f;
+
+
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+	}
+
+
+    private void FixedUpdate()
+    {
+        rotationY += Input.GetAxis("Mouse X") * sensitivityY;
+        rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
+
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+        transform.localEulerAngles = new Vector3(0, rotationY, 0);
+        cam.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+
+        
+
+    }
+}
---
+++ ProjectNeon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody Rigid;
+    public float MoveSpeed;
+    public float JumpForce;
+
+
+    private void FixedUpdate()
+    {
+        Move();
+
+        if (Input.GetKey(KeyCode.Space))
+            Rigid.AddForce(transform.up * JumpForce);
+    }
+
+    public void Move()
+    {
+        {
+            Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
+        }
+    }
+
+}
