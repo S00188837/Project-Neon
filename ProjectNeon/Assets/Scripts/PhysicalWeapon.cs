f41cf1dc8654c625797235ccf13023350092645a
Domas Ruginis <S00188837@mail.itsligo.ie>
2020-04-02 21:21:07 +0100

    CleanedUp

--- ProjectNeon/Assets/Scripts/PhysicalWeapon.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PhysicalWeapon : Weapon
-{
-    public Bullet BulletPrefab;
-}
--- ProjectNeon/Assets/Scripts/PlayerShooting/RayCastWeapon.cs
+++ ProjectNeon/Assets/Scripts/PlayerShooting/RayCastWeapon.cs
@@ -12,4 +12,5 @@ public class RayCastWeapon : Weapon
     {
         healthComponent.ApplyDamage(DamagePerHit);
     }
+
 }
