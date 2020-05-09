6e16ae5e839e289f972d600b9ddcaa6c1a37bb4e
Domas Ruginis <S00188837@mail.itsligo.ie>
2020-05-09 14:24:18 +0100

    Enemy Follow Done

--- ProjectNeon/Assets/Scripts/Domey/EnemyFollow.cs
+++ ProjectNeon/Assets/Scripts/Domey/EnemyFollow.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFollow : MonoBehaviour
+public class EnemyFollow : NavMeshMover
 {
-    // Start is called before the first frame update
-    void Start()
+    public string tagToTrack = "Player";
+    GameObject trackPlayer;
+    public float trackingDistance = 2;
+
+    
+    public override void Start()
     {
-        
+        trackPlayer = GameObject.FindGameObjectWithTag(tagToTrack);
+        base.Start();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (trackPlayer != null)
+        {
+            if (Vector3.Distance(transform.position, trackPlayer.transform.position) <= trackingDistance)
+            {
+                Resume();
+                MoveTo(trackPlayer);
+            }
+            else
+            {
+                Stop();
+            }
+        }
+    }
+
+    public override void OnDrawGizmos()
+    {
+        Gizmos.color = DebugLineColor;
+        Gizmos.DrawWireSphere(transform.position, trackingDistance);
+        base.OnDrawGizmos();
     }
 }
